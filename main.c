6308eb4adf00b8f581987232a261fb784133adcf
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-07-08 19:11:38 +0100

    in herdoc step

--- main.c
+++ main.c
@@ -6,15 +6,63 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 04:24:26 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/07/08 12:19:54 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/07/08 19:10:08 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void her_doc_handling(t_token **token_lst)
+{
+    t_token *tmp;
+    t_token *tmp2;
+    t_token *tmp3;
+    char *line;
+    int i;
+    int fd;
 
-
-
+    if (!token_lst || !*token_lst)
+        return;
+    i = 0;
+    tmp = *token_lst;
+    if (!tmp->next)
+        return;
+    fd = open(ft_strjoin("/tmp/",tmp->next->value), O_CREAT | O_RDWR | O_TRUNC, 0644);
+    while (tmp)
+    {
+        if (tmp->type == 2 && strncmp(tmp->value, "<<", 2) == 0)
+        {
+            while (1)
+            {
+                line = readline(">");
+                if (!line || ft_strncmp(line, tmp->next->value, ft_strlen(line)) == 0)
+                    break;
+                write(fd, line, ft_strlen(line));
+                write(fd, "\n", 1);
+            }
+            close(fd);
+            tmp2 = tmp->prev;
+            if (tmp->next)
+                tmp3 = tmp->next->next;
+            else
+                tmp3 = NULL;
+            if (tmp->value)
+                free(tmp->value);
+            if (tmp->next && tmp->next->value)
+                free(tmp->next->value);
+            if (tmp->next)
+                free(tmp->next);
+            free(tmp);
+            if (tmp2)
+                tmp2->next = tmp3;
+            if (tmp3)
+                tmp3->prev = tmp2;
+            tmp = tmp3;
+        }
+        if (tmp)
+            tmp = tmp->next;
+    }
+}
 void    readline_loop(char **line, t_gc **lst,char **env)
 {
     t_token *token_lst;
@@ -36,6 +84,7 @@ void    readline_loop(char **line, t_gc **lst,char **env)
 		// 	printf("%s\n", token[i]);
 		token = ft_tokinize(*line);
         syntax_error(token, &token_lst);
+        her_doc_handling(&token_lst);
         env_handling(&token_lst, env);
 		// ft_builtin_func(token);
 		// if (ft_strcmp(token[0], "echo") == 0)
