704061ac495694f56c172837c4c0a5d069336b70
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-08-19 23:09:30 +0100

    fixing error in herdoc

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 04:24:26 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/08/19 16:26:13 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/08/19 23:01:52 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,4 +124,5 @@ int	main(int ac, char **av, char **env)
 	tcsetattr(STDIN_FILENO, TCSANOW, &term_orig);
 	ft_free(&lst);
 	return (0);
+	
 }
